b4b4afdc3409398680e3c7690ce68c3bed3da525
DylanBilly <dylanbilly1988@gmail.com>
2017-04-19 15:44:14 +0200

    First Commit, Init Retrofit Android Application

---
+++ AndroidRetrofit/app/src/main/java/app/dgandroid/eu/androidretrofit/Activities/MainActivity.java
@@ -0,0 +1,14 @@
+package app.dgandroid.eu.androidretrofit.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import app.dgandroid.eu.androidretrofit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
